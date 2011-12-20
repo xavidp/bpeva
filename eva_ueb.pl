89e15accfbd8889542d0b008012a5f5786806179
Xavi <xavi@camagroc>
2011-12-20 18:22:51 +0100

    fixed issues with number of arguments and renamed print_ok to print_done

--- eva_ueb.pl
+++ eva_ueb.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 #
-# SCRIPT: eva_ueb.pl
+# SCRIPT: eva_ueb.plexit
 # Input: Directories of input and output
 # Output: List of Variants
 # Autor: Xavier de Pedro, Alex Sanchez (2011)
@@ -19,9 +19,11 @@ use Term::ANSIColor;
 my $program_ueb = "eva_ueb.pl";
 my $step_n = 0; # Step number for the loop inside each file to process
 my $file_n = 0; # File number for the loop of file to process
+#my $indexing = 0; # Param to indicate whether indexing the reference gnome is needed or not 
 my $step_tmp = $step_n; # dummy counter for the number of step when inside the files loop
 my $command00;
 my $now = nownice(time);
+my $genome_dir = "/home/xavi/Data/Data_Genomes/hg19/hg19.fa";
 
 
 ## Header shown at program execution
@@ -41,13 +43,18 @@ print_doc("\nThis program $program_ueb accepts 3 arguments passed to the program
 ## Step 1. Fetch the directory name
 $step_n = 1;
 my ($directory_in,$directory_out,$indexing,$n_arguments);
-$indexing = 0;
-$n_arguments = scalar(@ARGV);
-if ($n_arguments < 2 || $n_arguments >= 3) {
+$n_arguments = scalar(keys @ARGV); # with 'scalar(keys @ARGV);' I was misscounting 3 when 3 arguments where passed 
+
+if ($n_arguments < 2 || $n_arguments > 3) {
 	print_error("Wrong number of parameters: source and destination directories missing");
+} elsif ($n_arguments == 2) {
+	($directory_in,$directory_out) = @ARGV;
+	$indexing = "off"; # Set indexing to 0 (off)
+}else{ # case when there are 3 arguments
+	($directory_in,$directory_out,$indexing) = @ARGV;
 }
-($directory_in,$directory_out,$indexing) = @ARGV;
-print_ok("$now - Step $step_n. Reading parameters");
+
+print_done("$now - Step $step_n. Reading parameters");
 
 ## Step 2. Accesing directory
 $step_n = $step_n+1;
@@ -57,7 +64,7 @@ my ($name,$filter,$command,$options);
 opendir(DIR,$directory_in);
 #opendir(DIR,$directory_in) or die "Cannot open $directory_in: !";
 #opendir my $dh, $dir_to_process or die "Cannot open $dir_to_process: $!";
-print_ok("$now - Step $step_n. Accesing directory: $directory_in");
+print_done("$now - Step $step_n. Accesing directory: $directory_in");
 
 while ($file = readdir(DIR))
 {
@@ -78,13 +85,13 @@ while ($file = readdir(DIR))
 	print_doc("$now -   Step $step_n.$step_tmp Quality Control and Preprocessing: $name ...");
 	$file_in = "$directory_in/$file";
 #	$file_out = "$directory_out/$name.txt";
-	$options = "--outdir=$directory_out";
 	$command00 = "/home/ueb/fastqc/fastqc"; # path to fastqc binary; adapt to your case.
 #	$command00 = "ls"; # next command.
-	$options = "$file_in";
+	$options = "$file_in --outdir=$directory_out ";
 	$command = "$command00 $options";
 	system($command);
-	print_ok("$now -\t\t\t\t\t");
+	print_done("$now -\t\t\t\t\t");
+
 
 
 	## Next Step. Map against reference genome: Index the reference genome (if needed)
@@ -92,19 +99,31 @@ while ($file = readdir(DIR))
 	print_doc("$now -   Step $step_n.$step_tmp Map against reference genome: Index the reference genome (if needed): $name ...");
 	# Index the reference genome, if requested with param "-index"
 #	if (($ARGV[2] eq '-index') || ($ARGV[3] eq '-index')) { # index the reference genome
-	if (($indexing eq '-index') || ($indexing eq '-Index')) { # index the reference genome
-	#	$file_in = "$directory_in/$name";
-	#	$file_out = "$directory_out/$name.txt";
-		$command00 = "bwa index"; # next command.
-		$options = " -a bwtsw /home/xavi/Data/Data_Genomes/hg19/hg19.fa";
-	}
-	else { # skip the indexing of the reference genome
-		$command00 = "echo '  ...skipped...'"; # next command.
-		$options = "";
-	}
+#	if ($n_arguments == 3) { # only when there are 3 arguments use $indexing; otherwise,  a warning of uninitialized $indexing is shown 
+		if (($indexing eq '-index') || ($indexing eq '-Index')) { # index the reference genome
+		#	$file_in = "$directory_in/$name";
+		#	$file_out = "$directory_out/$name.txt";
+			$command00 = "bwa index"; # next command.
+			$options = " -a bwtsw $genome_dir";
+		}
+		else { # skip the indexing of the reference genome
+			$command00 = "echo '  ...skipped...'"; # next command.
+			$options = "";
+		}
+#	} # end of condition for when there are 3 arguments
 	$command = "$command00 $options";
 	system($command);
-	print_ok("$now -\t\t\t\t\t");
+	print_done("$now -\t\t\t\t\t");
+
+
+## XXX debugging ini...
+##print_doc("@ARGV"); # xxx debug
+#print_doc("@ARGV[0]"); # xxx debug
+#print_doc("@ARGV[1]"); # xxx debug
+#print_doc("@ARGV[2]"); # xxx debug
+##$n_arguments = scalar(keys @ARGV); # xxx debug
+#print_doc("$n_arguments") ; # xxx debug
+#exit(0); # XXX debugging end...
 
 
 	## Next Step. Map against reference genome: do the mapping
@@ -116,7 +135,7 @@ while ($file = readdir(DIR))
 	$options = " /home/xavi/Data/Data_Genomes/hg19/hg19.fa $file_in > $file_out";
 	$command = "$command00 $options";
 	system($command);
-	print_ok("$now -\t\t\t\t\t");
+	print_done("$now -\t\t\t\t\t");
 
 
 	## Next Step. Convert sam to bam and sort it
@@ -128,7 +147,7 @@ while ($file = readdir(DIR))
 	$options = " view -bS $file_in | samtools sort - $file_out"; # Attention: it seems that the real file generated is a .bam file
 	$command = "$command00 $options";
 	system($command);
-	print_ok("$now -\t\t\t\t\t");
+	print_done("$now -\t\t\t\t\t");
 
 
 	## Next Step. Remove possible PCR duplicates
@@ -140,7 +159,7 @@ while ($file = readdir(DIR))
 	$options = " rmdup -s $file_in $file_out";
 	$command = "$command00 $options";
 	system($command);
-	print_ok("$now -\t\t\t\t\t");
+	print_done("$now -\t\t\t\t\t");
 
 
 
@@ -153,7 +172,7 @@ while ($file = readdir(DIR))
 	$options = " index $file_in";
 	$command = "$command00 $options";
 	system($command);
-	print_ok("$now -\t\t\t\t\t");
+	print_done("$now -\t\t\t\t\t");
 
 
 
@@ -166,7 +185,7 @@ while ($file = readdir(DIR))
 	$options = " flagstat $file_in";
 	$command = "$command00 $options";
 	system($command);
-	print_ok("$now -\t\t\t\t\t");
+	print_done("$now -\t\t\t\t\t");
 
 
 
@@ -179,7 +198,7 @@ while ($file = readdir(DIR))
 	$options = " mpileup -uf /home/xavi/Data/Data_Genomes/hg19/hg19.fa $file_in | bcftools view -vcg - > $file_out";
 	$command = "$command00 $options";
 	system($command);
-	print_ok("$now -\t\t\t\t\t");
+	print_done("$now -\t\t\t\t\t");
 # testing
 # samtools mpileup -uf /home/xavi/Data/Data_Genomes/hg19/hg19.fa ./dir_out/prova1.sam.sorted.noDup.bam | bcftools view -vcg - > ./dir_out/prova1.sam.sorted.noDup.bam.samtools.var.raw.vcf
 
@@ -193,7 +212,7 @@ while ($file = readdir(DIR))
 	$options = " varFilter -Q 10 -d 15 -a 5 $file_in > $file_out";
 	$command = "$command00 $options";
 	system($command);
-	print_ok("$now -\t\t\t\t\t");
+	print_done("$now -\t\t\t\t\t");
 
 
 
@@ -206,7 +225,7 @@ while ($file = readdir(DIR))
 	$options = " $file_in -format vcf4 > $file_out";
 	$command = "$command00 $options";
 	system($command);
-	print_ok("$now -\t\t\t\t\t");
+	print_done("$now -\t\t\t\t\t");
 
 
 
@@ -219,7 +238,7 @@ while ($file = readdir(DIR))
 	$options = "";
 	$command = "$command00 $options";
 	system($command);
-	print_ok("$now -\t\t\t\t\t");
+	print_done("$now -\t\t\t\t\t");
 
 
 
@@ -232,7 +251,7 @@ while ($file = readdir(DIR))
 	$options = "";
 	$command = "$command00 $options";
 	system($command);
-	print_ok("$now -\t\t\t\t\t");
+	print_done("$now -\t\t\t\t\t");
 
 }
 
@@ -240,7 +259,7 @@ while ($file = readdir(DIR))
 closedir(DIR);
 
 $step_n = $step_n+1;
-print_ok("$now - Step $step_n. End of EVA UEB pipeline");
+print_done("$now - Step $step_n. End of EVA UEB pipeline");
 exit(0);
 
 ##########################
@@ -257,14 +276,14 @@ sub print_doc
 	print STDERR color(" reset ");
 }
 
-sub print_ok
+sub print_done
 {
 	my $mess;
 	$mess = $_[0];
 	$now = nownice(time);
 
 	print STDERR color("bold green"),
-		"$mess\t\t[OK]\n\n";
+		"$mess\t\t[DONE]\n\n";
 	print STDERR color(" reset ");
 
 }
